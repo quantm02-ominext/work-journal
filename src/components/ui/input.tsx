1d77a0cf4e2e9a17e3bc382a89193897fb0ad10e
quantm02 <quantm02@ominext.com>
2025-03-21 16:02:24 +0700

    feat(input): support icons

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,29 +1,72 @@
 'use client'
 
+import { Slot } from '@radix-ui/react-slot'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-import { useFieldContext } from './field'
+interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+	startElement?: React.ReactNode
+	endElement?: React.ReactNode
+	asChild?: boolean
+	startElementProps?: React.ComponentPropsWithoutRef<'div'>
+	endElementProps?: React.ComponentPropsWithoutRef<'div'>
+}
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-	({ className, type, onFocus, onBlur, ...props }, ref) => {
-		const fieldContext = useFieldContext(false)
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+	(
+		{
+			className,
+			startElement,
+			endElement,
+			startElementProps,
+			endElementProps,
+			asChild = false,
+			disabled,
+			...props
+		},
+		ref,
+	) => {
+		const Comp = asChild ? Slot : 'input'
 
 		return (
-			<input
-				type={type}
+			<div
 				className={cn(
-					'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-					className,
+					'flex w-full rounded-md border border-input bg-background text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring',
 				)}
-				ref={ref}
-				{...fieldContext?.getControlProps({
-					onBlur,
-					onFocus,
-				})}
-				{...props}
-			/>
+			>
+				{startElement && (
+					<div
+						className={cn(
+							'flex min-w-10 shrink-0 items-center justify-center px-3 text-muted-foreground',
+							disabled && 'cursor-not-allowed opacity-50',
+							startElementProps?.className,
+						)}
+					>
+						{startElement}
+					</div>
+				)}
+				<Comp
+					className={cn(
+						'flex-1 border-0 bg-transparent px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+						className,
+					)}
+					ref={ref}
+					disabled={disabled}
+					{...props}
+				/>
+				{endElement && (
+					<div
+						className={cn(
+							'flex min-w-10 shrink-0 items-center justify-center px-3 text-muted-foreground',
+							disabled && 'cursor-not-allowed opacity-50',
+							endElementProps?.className,
+						)}
+					>
+						{endElement}
+					</div>
+				)}
+			</div>
 		)
 	},
 )
